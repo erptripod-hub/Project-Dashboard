5a39e9004c10ab958190fbf5b42bd91300f01087
ERP Tripod <erp@tripodmena.com>
2026-03-24 07:06:50 +0000

    fix: auto-populate checklist and drawing packages on new form via JS onload

--- project_dashboard/project_dashboard/doctype/technical_drawing_request/technical_drawing_request.js
+++ project_dashboard/project_dashboard/doctype/technical_drawing_request/technical_drawing_request.js
@@ -1,16 +1,58 @@
 frappe.ui.form.on("Technical Drawing Request", {
 
 	onload: function(frm) {
-		// Auto set requested_by from logged in employee
 		if (frm.is_new()) {
+			// Auto set requested_by
 			frappe.db.get_value("Employee", {"user_id": frappe.session.user}, "name", function(r) {
 				if (r && r.name) frm.set_value("requested_by", r.name);
 			});
+
+			// Auto populate checklist
+			var checklist_items = [
+				"Concept/3D Renders",
+				"Initial Architectural Drawings",
+				"Initial MEP Drawings",
+				"Unpriced BOQ (With Scope Highlighted)",
+				"Material Specifications/Samples",
+				"Site Survey Report",
+				"Site Survey Sketch - where applicable",
+				"LOD"
+			];
+
+			frm.clear_table("initiation_checklist");
+			checklist_items.forEach(function(item) {
+				var row = frm.add_child("initiation_checklist");
+				row.particular = item;
+				row.status = "NA";
+			});
+			frm.refresh_field("initiation_checklist");
+
+			// Auto populate drawing packages
+			var drawing_packages = [
+				"Demolition Plan",
+				"General Arrangement Plan",
+				"Floor Plan & RCP",
+				"Elevations & Sections",
+				"JOINERY - Fixed",
+				"JOINERY - Free Standing",
+				"Shopfront",
+				"Electrical & Plumbing",
+				"HVAC, Fire Fighting & Fire Alarm",
+				"IT Works - CCTV, Data",
+				"Others - If any"
+			];
+
+			frm.clear_table("drawing_packages");
+			drawing_packages.forEach(function(pkg) {
+				var row = frm.add_child("drawing_packages");
+				row.drawing_package = pkg;
+				row.drawing_status = "Pending";
+			});
+			frm.refresh_field("drawing_packages");
 		}
 	},
 
 	refresh: function(frm) {
-		// Status indicator colors
 		var status_color = {
 			"Draft": "gray",
 			"Submitted to Design": "blue",
@@ -22,100 +64,50 @@ frappe.ui.form.on("Technical Drawing Request", {
 			frm.page.set_indicator(frm.doc.status, status_color[frm.doc.status] || "gray");
 		}
 
-		// Design Manager: button to mark Acknowledged
-		if (
-			frm.doc.docstatus === 1 &&
-			frm.doc.status === "Submitted to Design" &&
-			frappe.user.has_role("Design Manager")
-		) {
+		if (frm.doc.docstatus === 1 && frm.doc.status === "Submitted to Design" && frappe.user.has_role("Design Manager")) {
 			frm.add_custom_button(__("Acknowledge"), function() {
-				frappe.confirm(
-					"Mark this TDR as Acknowledged?",
-					function() {
-						frappe.call({
-							method: "frappe.client.set_value",
-							args: {
-								doctype: "Technical Drawing Request",
-								name: frm.doc.name,
-								fieldname: "status",
-								value: "Acknowledged"
-							},
-							callback: function() {
-								frm.reload_doc();
-								frappe.msgprint("TDR Acknowledged successfully.", "green");
-							}
-						});
-					}
-				);
+				frappe.confirm("Mark this TDR as Acknowledged?", function() {
+					frappe.call({
+						method: "frappe.client.set_value",
+						args: { doctype: "Technical Drawing Request", name: frm.doc.name, fieldname: "status", value: "Acknowledged" },
+						callback: function() { frm.reload_doc(); }
+					});
+				});
 			}, __("Actions")).addClass("btn-primary");
 		}
 
-		// Design Manager: mark as In Progress
-		if (
-			frm.doc.docstatus === 1 &&
-			frm.doc.status === "Acknowledged" &&
-			frappe.user.has_role("Design Manager")
-		) {
+		if (frm.doc.docstatus === 1 && frm.doc.status === "Acknowledged" && frappe.user.has_role("Design Manager")) {
 			frm.add_custom_button(__("Mark In Progress"), function() {
 				frappe.call({
 					method: "frappe.client.set_value",
-					args: {
-						doctype: "Technical Drawing Request",
-						name: frm.doc.name,
-						fieldname: "status",
-						value: "In Progress"
-					},
+					args: { doctype: "Technical Drawing Request", name: frm.doc.name, fieldname: "status", value: "In Progress" },
 					callback: function() { frm.reload_doc(); }
 				});
 			}, __("Actions"));
 		}
 
-		// Design Manager: mark as Completed
-		if (
-			frm.doc.docstatus === 1 &&
-			["Acknowledged", "In Progress"].includes(frm.doc.status) &&
-			frappe.user.has_role("Design Manager")
-		) {
+		if (frm.doc.docstatus === 1 && ["Acknowledged","In Progress"].includes(frm.doc.status) && frappe.user.has_role("Design Manager")) {
 			frm.add_custom_button(__("Mark Completed"), function() {
-				frappe.confirm(
-					"Mark all drawings as received and complete this TDR?",
-					function() {
-						frappe.call({
-							method: "frappe.client.set_value",
-							args: {
-								doctype: "Technical Drawing Request",
-								name: frm.doc.name,
-								fieldname: "status",
-								value: "Completed"
-							},
-							callback: function() { frm.reload_doc(); }
-						});
-					}
-				);
+				frappe.confirm("Mark this TDR as Completed?", function() {
+					frappe.call({
+						method: "frappe.client.set_value",
+						args: { doctype: "Technical Drawing Request", name: frm.doc.name, fieldname: "status", value: "Completed" },
+						callback: function() { frm.reload_doc(); }
+					});
+				});
 			}, __("Actions"));
 		}
-
-		// Drawing package - auto set received date when status = Drawing Received
-		frm.fields_dict["drawing_packages"].grid.get_field("drawing_status").df.onchange = function() {
-			var row = frm.fields_dict["drawing_packages"].grid.get_selected_children()[0];
-			if (row && row.drawing_status === "Drawing Received" && !row.received_date) {
-				frappe.model.set_value(row.doctype, row.name, "received_date", frappe.datetime.get_today());
-			}
-		};
 	},
 
 	project: function(frm) {
 		if (frm.doc.project) {
 			frappe.db.get_value("Project", frm.doc.project, ["project_name", "custom_project_location"], function(r) {
-				if (r) {
-					if (r.custom_project_location) frm.set_value("project_location", r.custom_project_location);
-				}
+				if (r && r.custom_project_location) frm.set_value("project_location", r.custom_project_location);
 			});
 		}
 	}
 });
 
-// Auto set received_date when drawing status changes to received
 frappe.ui.form.on("TDR Drawing Package", {
 	drawing_status: function(frm, cdt, cdn) {
 		var row = locals[cdt][cdn];
