57f6368f5dfc6c47633fc762a1635e7c204ef004
ERP Tripod <erp@tripodmena.com>
2026-03-24 12:05:00 +0000

    fix: remove override_doctype_dashboards - restores all Project connections

--- project_dashboard/hooks.py
+++ project_dashboard/hooks.py
@@ -6,7 +6,3 @@ app_publisher = "ERP Tripod"
 app_description = "Project Dashboard for ERPNext v14"
 app_email = "erp@tripodmena.com"
 app_license = "MIT"
-
-override_doctype_dashboards = {
-	"Project": "project_dashboard.project_dashboard.doctype.technical_drawing_request.technical_drawing_request.get_project_dashboard_data"
-}
